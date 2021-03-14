d4afa0dba46a53f8a10085a8eb21b475c0eb6056
Justin Jang <T_Jang@baylor.edu>
2021-03-14 17:58:40 -0500

    Creating Settings

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -1,13 +1,15 @@
 import sys
 import pygame
-
+from settings import Settings
 class AlienInvasion:
     #Overall class to manage game assets and behavior
 
     def __init__(self):
         #initialize the game and create the game resources
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+            #old settings code: self.screen = pygame.display.set_mode((1200,800))
             #the (1200,800) is a tuple that defines the dimensions of the game window
             #this means that the window will be 1200 pixels wide by 800 pixels high
         pygame.display.set_caption("Alien Invasion")
@@ -22,7 +24,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit
             #Redraw the screen during each pass through the loop:
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             #make the most recently drawn screen visible.
             pygame.display.flip()
 
