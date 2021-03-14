dcadcdbd0f148db469a312fa70c502b4ef2f3829
Justin Jang <T_Jang@baylor.edu>
2021-03-14 18:55:41 -0500

    Adding Left and Right movement keys

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -29,14 +29,21 @@ class AlienInvasion:
         #watch for kbm events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit
+                sys.exit()
+
             elif event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT:
                     #Move the ship right
                     self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
     def _update_screen(self):
         #Redraw the screen during each pass through the loop:
         self.screen.fill(self.settings.bg_color)
