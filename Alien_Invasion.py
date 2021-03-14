49a5089ba015c8305ac27ddf27460f8460b073b0
Justin Jang <T_Jang@baylor.edu>
2021-03-14 18:35:08 -0500

    Adding right input movements

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         #Start the main loop for the game
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             
     def _check_events(self):
@@ -32,7 +33,10 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.type == pygame.K_RIGHT:
                     #Move the ship right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
     def _update_screen(self):
         #Redraw the screen during each pass through the loop:
         self.screen.fill(self.settings.bg_color)
