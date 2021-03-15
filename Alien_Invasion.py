cbfb834163675839aeb047527c8a17b6ca0aaa03
Justin Jang <T_Jang@baylor.edu>
2021-03-14 23:58:57 -0500

    Increasing the speed with level ups and reseting

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -71,6 +71,9 @@ class AlienInvasion:
         #start a new game when the player clicks play
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            #reset game settings
+            self.settings.initialize_dynamic_settings()
+
             #reset game stats
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -130,6 +133,7 @@ class AlienInvasion:
             #destroy existing bullets and create new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         #create the fleet
