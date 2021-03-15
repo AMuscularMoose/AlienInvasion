d94164aa98b186790580c52f22c8c23b5602960e
Justin Jang <T_Jang@baylor.edu>
2021-03-14 21:27:51 -0500

    Creating the fleet

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -91,17 +91,22 @@ class AlienInvasion:
         number_aliens_x = available_space_x // (2*alien_width)
 
         #determine the numbner of rows of aliens that fit on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3*alien_height) - ship_height)
+        number_rows = available_space_y // (2*alien_height)
 
-        #create the first row of aliens
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        #create the full fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number,row_number)
 
-    def _create_alien(self,alien_number):
+    def _create_alien(self,alien_number,row_number):
         #create an alien and put it in the row
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
