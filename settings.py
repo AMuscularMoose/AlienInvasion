cc296a950b0d99ddf774223c0949d4cffbbcb834
Justin Jang <T_Jang@baylor.edu>
2021-03-15 00:18:07 -0500

    updating scores

--- settings.py
+++ settings.py
@@ -30,6 +30,9 @@ class Settings:
         self.fleet_direction = 1
             #fleet_direction of 1 represents RIGHT while -1 represents LEFT
 
+        #scoring
+        self.alien_points = 50
+
     def increase_speed(self):
         """Increase speed settings"""
         self.ship_speed *= self.speedup_scale
