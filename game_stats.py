6772e96d13a59880fc879d1c36d76392e51a1bea
Justin Jang <T_Jang@baylor.edu>
2021-03-15 00:42:12 -0500

    High scores

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats:
         #start the game in an inactive state
         self.game_active = False
 
+        #high score should never be reset
+        self.high_score = 0
+
     def reset_stats(self):
         #initialize stats that can change during game
         self.ships_left = self.settings.ship_limit
