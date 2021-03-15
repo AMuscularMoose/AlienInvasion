2ef79dd81c947c02bfd391989fad9492b8535d67
Justin Jang <T_Jang@baylor.edu>
2021-03-14 23:19:18 -0500

    Creating the button

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 import pygame.font
 
-class Botton:
+class Button:
 
     def __init__(self,ai_game,msg):
         #init button attrib
@@ -9,9 +9,11 @@ class Botton:
 
         #set the dimensions and prop of the bottom
         self.width, self.height = 200, 50
-        self.botton_color = (0,255,0)
+        self.button_color = (0,255,0)
         self.text_color = (255,255,255)
         self.font = pygame.font.Sysfont(None,48)
+            #the None tells pygame to use the default font
+            #the 48 specifies the font size
 
         #build the buttons rect object and center it
         self.rect = pygame.Rect(0,0,self.width,self.height)
@@ -19,3 +21,16 @@ class Botton:
 
         #the buttom message needs to be prepped only once
         self._prep_msg(msg)
+
+    def _prep_msg(self,msg):
+        #turn msg into a rendered image and center text on the button
+        self.msg_image = self.font.render(msg,True,self.text_color,self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        #draw blank button and then draw message
+        self.screen.fill(self.button_color,self.rect)
+            #this is to draw the rectangle portion of the buttom
+        self.screen.blit(self.msg_image,self.msg_image_rect)
+            #this is to draw the text image to the screen, passing it an image and the rect object associated with it.
